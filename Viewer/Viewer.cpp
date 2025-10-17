082099e8656f9367723d8d0a10ef89b36aebf8b9
ipz233kds <ipz233_kds@student.ztu.edu.ua>
2025-10-17 23:45:57 +0300

    Реалізовано візуалізатор з таймером

---
+++ Viewer/Viewer.cpp
@@ -0,0 +1,156 @@
+﻿/*******************************************************************************
+ * Файл:    Viewer.cpp (Проект: Viewer)
+ * Призначення: Віконна програма. Відкриває проекцію файлу та за таймером
+ * відображає її вміст у вигляді "стовпчиків" із зірочок.
+ * Автор: Круковський Данило
+ ******************************************************************************/
+
+#include "framework.h"
+#include "Viewer.h"
+#include <windows.h>
+#include "../common.h"
+
+HANDLE hMapFile;
+HANDLE hMutex;
+int* pData;
+int localData[ARRAY_SIZE];
+
+ATOM MyRegisterClass(HINSTANCE hInstance);
+BOOL InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
+    MyRegisterClass(hInstance);
+    if (!InitInstance(hInstance, nCmdShow)) {
+        return FALSE;
+    }
+
+    MSG msg;
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return (int)msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance) {
+    WNDCLASSEXW wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = L"VIEWER_CLASS";
+    wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
+    HWND hWnd = CreateWindowW(L"VIEWER_CLASS", L"Viewer", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, 400, 600, nullptr, nullptr, hInstance, nullptr);
+    if (!hWnd) {
+        return FALSE;
+    }
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+    return TRUE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_CREATE:
+    {
+        SetWindowText(hWnd, L"Візуалізатор");
+
+        hMutex = OpenMutex(MUTEX_ALL_ACCESS, FALSE, MUTEX_NAME);
+        if (hMutex == NULL) {
+            MessageBox(hWnd, L"Не вдалося відкрити м'ютекс! Спочатку запустіть Генератор/Сортувальник.", L"Помилка", MB_OK);
+            PostQuitMessage(1);
+            return -1;
+        }
+
+        hMapFile = OpenFileMapping(
+            FILE_MAP_READ,
+            FALSE,
+            MAP_OBJECT_NAME
+        );
+
+        if (hMapFile == NULL) {
+            MessageBox(hWnd, L"Не вдалося відкрити проекцію файлу! Спочатку запустіть Сортувальник.", L"Помилка", MB_OK);
+            CloseHandle(hMutex);
+            PostQuitMessage(1);
+            return -1;
+        }
+
+        pData = (int*)MapViewOfFile(hMapFile, FILE_MAP_READ, 0, 0, ARRAY_SIZE * sizeof(int));
+        if (pData == NULL) {
+            MessageBox(hWnd, L"Не вдалося відобразити файл у пам'ять!", L"Помилка", MB_OK);
+            CloseHandle(hMapFile);
+            CloseHandle(hMutex);
+            PostQuitMessage(1);
+            return -1;
+        }
+
+        SetTimer(hWnd, TIMER_ID, 500, NULL);
+        break;
+    }
+    case WM_TIMER:
+        if (wParam == TIMER_ID) {
+
+            WaitForSingleObject(hMutex, INFINITE);
+            __try {
+                memcpy(localData, pData, sizeof(localData));
+            }
+            __except (EXCEPTION_EXECUTE_HANDLER) {
+            }
+            ReleaseMutex(hMutex);
+
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        break;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+
+        int yPos = 10;
+        wchar_t starBuffer[102];
+
+        for (int i = 0; i < ARRAY_SIZE; ++i) {
+            int count = localData[i];
+            if (count > 100) count = 100;
+
+            int j;
+            for (j = 0; j < count; ++j) {
+                starBuffer[j] = L'*';
+            }
+            starBuffer[j] = L'\0';
+
+            TextOut(hdc, 10, yPos, starBuffer, j);
+            yPos += 20;
+        }
+
+        EndPaint(hWnd, &ps);
+        break;
+    }
+    case WM_DESTROY:
+        KillTimer(hWnd, TIMER_ID);
+        if (pData) UnmapViewOfFile(pData);
+        if (hMapFile) CloseHandle(hMapFile);
+        if (hMutex) CloseHandle(hMutex);
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// --- Кінець файлу Viewer.cpp (Viewer) ---
