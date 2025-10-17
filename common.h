9797b9e7bfe79e7a4a3f1e339a63086b2ace9560
ipz233kds <ipz233_kds@student.ztu.edu.ua>
2025-10-17 19:38:17 +0300

    Початкова структура рішення з 3 проектами

---
+++ common.h
@@ -0,0 +1,19 @@
+﻿/*******************************************************************************
+ * Файл:    common.h
+ * Призначення: Спільний заголовоний файл з константами для всіх програм
+ * Автор:   Круковський Данило
+ ******************************************************************************/
+
+#pragma once
+
+#define DATA_FILENAME L"data.dat"
+
+#define MAP_OBJECT_NAME L"MySharedMemoryLab"
+
+#define MUTEX_NAME L"MySharedDataMutex"
+
+#define ARRAY_SIZE 25
+
+#define TIMER_ID 1
+
+// --- Кінець файлу common.h ---
